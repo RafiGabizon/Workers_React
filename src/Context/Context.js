12d85ddea3ff3abac95c6da1845c0bbc11c9a640
Rafi Gabizon <109092985+RafiGabizon@users.noreply.github.com>
2024-07-05 09:55:01 +0300

    Add files via upload

---
+++ src/Context/Context.js
@@ -0,0 +1,50 @@
+import { createContext, useEffect, useState } from 'react';
+
+export const AppContext = createContext();
+
+const ContextProvider = ({ children }) => {
+  const [favourites, setFavourites] = useState([]);
+
+  useEffect(() => {
+    const loadFavourites = () => {
+      const savedFavourites = localStorage.getItem('favourites');
+      if (savedFavourites) {
+        setFavourites(JSON.parse(savedFavourites));
+      }
+    };
+
+    loadFavourites();
+  }, []);
+
+  const addFavourite = (employee) => {
+    setFavourites((prevFavourites) => {
+      const isAlreadyFavourited = prevFavourites.some((fav) => fav.login.uuid === employee.login.uuid);
+      
+      if (isAlreadyFavourited) {
+        alert('Already in favourites');
+        return prevFavourites;
+      }
+
+      const updatedFavourites = [...prevFavourites, employee];
+      localStorage.setItem('favourites', JSON.stringify(updatedFavourites));
+      return updatedFavourites;
+    });
+  };
+
+  const removeFavourite = (employee) => {
+ 
+    const updatedFavourites = favourites.filter((fav) => fav.login.uuid !== employee.login.uuid);
+    localStorage.setItem('favourites', JSON.stringify(updatedFavourites));
+    setFavourites(updatedFavourites);
+  };
+
+  const contextValue = { favourites, addFavourite, removeFavourite };
+
+  return (
+    <AppContext.Provider value={contextValue}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default ContextProvider;
